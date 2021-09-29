392beede06190b21993f9ad1d7e9809354c4b7c7
yaeoni <tjghwnd103@gmail.com>
2021-09-29 14:32:03 +0900

    [FEAT] local 에서 파일 크기 구하는 것 추가

--- local_resize/local_resize.js
+++ local_resize/local_resize.js
@@ -2,14 +2,21 @@ const sharp = require("sharp");
 const fs = require("fs");
 const sizeOf = require("image-size");
 
-const dimensions = sizeOf("test.png");
+const file = "파일 이름;
+const dimensions = sizeOf(file);
 
-sharp("test.png")
-  .resize(dimensions.width / 3, dimensions.height / 3) // width, height pixel
+// local에서 파일 크기 구하기
+var stats = fs.statSync(file);
+var fileSizeInBytes = stats.size;
+var fileSizeInKilobytes = fileSizeInBytes / 1024;
+
+
+sharp(file)
+  .resize(dimensions.width / 2, dimensions.height / 2) // width, height pixel
   .toFormat("jpeg")
   .toBuffer()
   .then((data) => {
-    fs.writeFileSync("2.jpeg", data);
+    fs.writeFileSync("결과", data);
   })
   .catch((err) => {
     console.log(err);
